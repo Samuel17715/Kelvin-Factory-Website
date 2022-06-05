21a44a837c0129a082edc1edc748c21434d85c2b
Samuel Owolabi <owolabisamuel15@gmail.com>
2022-06-05 16:03:46 +0100

    New

--- assets/js/custom-js.js
+++ assets/js/custom-js.js
@@ -253,6 +253,7 @@ $('.materialTab li[data-tab]').on('click', function(){
     $(this).addClass('active');
     $('.materialTabContent[data-tab-content').removeClass('active');
     $('.materialTabContent[data-tab-content='+ $(this).data('tab') +']').addClass('active');
+    $('.materialTabContent[data-tab-content='+ $(this).data('tab') +']').addClass('animate__fadeInUp');
 });
 
 
